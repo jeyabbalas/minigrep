58c8b70a1b21d8b21433b0b9fda4982f8eae15e0
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2021-02-10 01:16:04 -0500

    Reading input file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -7,4 +9,8 @@ fn main() {
 
     println!("The input query string is: {}", query);
     println!("The file to be searched is: {}", filename);
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    println!("File contents:\n {}", contents);
 }

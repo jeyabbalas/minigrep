10d0a35bda6d9da350c9ed493cbc3ec221b15791
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2021-02-12 14:35:45 -0500

    added case-insensitive search

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Basic grep CLI tool in Rust
 This project implements a basic functionality grep CLI (command line interface) tool using Rust. This project is an exercise in [Chapter 12 of the official Rust book](https://doc.rust-lang.org/book/ch12-00-an-io-project.html).
 
-*Grep* (<u>g</u>lobally search a <u>r</u>egular <u>e</u>xpression and <u>p</u>rint) is a classic command line tool, whose primary function is to search for a user-input string within a given file, and print the lines that contains the queried string.
+*Grep* (**g**lobally search a **r**egular **e**xpression and **p**rint) is a classic command line tool, whose primary function is to search for a user-input string within a given file, and print the lines that contains the queried string.
+
+## Usage
+Download the executable binary at ```./target/release/minigrep```. Run it in command line using the Terminal app in a macOS system as follows.
+
+The above command runs case-sensitive search by default. To run case-insensitive search, set environment variable ```CASE_INSENSITIVE`` to any value, as shown below.
+
+```
+$> CASE_INSENSITIVE=1 minigrep string_query filename.txt
+```
 
 ## Implementation Steps
 1. Use [```args()``` a standard library function](https://doc.rust-lang.org/std/env/fn.args.html) to accept command line arguments. Function returns an iterator of input arguments. Collect it into a collection. Save arguments needed for grep.
@@ -11,5 +20,6 @@ This project implements a basic functionality grep CLI (command line interface) 
 5. Move program logic (reading file contents) from ```main()``` into ```run()```. Return a Result type that returns unit type if everything went well, else return an Error type (use trait object that implies any type with ```std::error::Error``` trait).
 6. Error handling in ```main()```: if ```run()``` returns an error, display a helpful message and exit gracefully.
 7. Move arguments parsing and program logic to ```lib.rs```.
-8. <u>Test-driven development</u>: Write a test to evaluate the as yet to be implemented method ```search()``` in ```lib.rs```, which returns lines in a text that contains a queried string. Implement an empty ```search()```, which fails. Implement minimal logic to pass the test.
+8. **Test-driven development**: Write a test to evaluate the as yet to be implemented method ```search()``` in ```lib.rs```, which returns lines in a text that contains a queried string. Implement an empty ```search()```, which fails. Implement minimal logic to pass the test.
 9. Call ```search()``` from ```run()```.
+10. Write tests for case-sensitive and -insensitive search function. Check if ```CASE_INSENSITIVE``` environment variable is set, if so, run case-insensitive search, else run case sensitive search.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{fs, error::Error};
+use std::{fs, env, error::Error};
 
 pub struct Config {
     query: String,
     filename: String,
+    case_insensitive: bool,
 }
 
 impl Config {
@@ -14,6 +15,7 @@ impl Config {
         let config = Self {
             query: args[1].clone(),
             filename: args[2].clone(),
+            case_insensitive: !env::var("CASE_INSENSITIVE").is_err(),
         };
 
         println!("The input query string is: {}", config.query);
@@ -26,7 +28,11 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
 
-    let matched = search(&config.query, &content);
+    let matched = if config.case_insensitive {
+        case_insensitive_search(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
 
     for line in matched {
         println!("{}", line);
@@ -35,11 +41,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let mut matched = Vec::new();
 
     for line in content.lines() {
         if line.contains(query) {
+            matched.push(line.trim());
+        }
+    }
+
+    matched
+}
+
+fn case_insensitive_search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut matched = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
             matched.push(line.trim());
         }
     }
@@ -59,10 +77,44 @@ mod tests {
         You are ungrammatical
         In speaking as you do,
         You should say, \"To whom! To whom!\"
-        Not, \"To who! To who\"";
+        Not, \"To who! To who!\"";
         
         let expected_result = vec!["You should say, \"To whom! To whom!\"",
-                                                          "Not, \"To who! To who\""];
+                                                          "Not, \"To who! To who!\""];
         assert_eq!(expected_result, search(test_query, test_content));
+    }
+
+    #[test]
+    fn test_case_insensitive_search() {
+        let query = "YOU";
+        let content = "Little owlet in the glen
+        I'm ashamed of you;
+        You are ungrammatical
+        In speaking as you do,
+        You should say, \"To whom! To whom!\"
+        Not, \"To who! To who!\"";
+
+        let expected_result = vec!["I'm ashamed of you;",
+                                                         "You are ungrammatical",
+                                                         "In speaking as you do,",
+                                                         "You should say, \"To whom! To whom!\""];
+
+        assert_eq!(expected_result, case_insensitive_search(query, content));
+    }
+
+    #[test]
+    fn test_case_sensitive_search() {
+        let query = "You";
+        let content = "Little owlet in the glen
+        I'm ashamed of you;
+        You are ungrammatical
+        In speaking as you do,
+        You should say, \"To whom! To whom!\"
+        Not, \"To who! To who!\"";
+
+        let expected_result = vec!["You are ungrammatical",
+                                                         "You should say, \"To whom! To whom!\""];
+
+        assert_eq!(expected_result, search(query, content));
     }
 }
